d5ed17b1872e8731474b5eca9815d20628bcc822
Tze Meng LEE <tzemenglee@Tzes-MacBook-Pro.local>
2020-04-29 17:20:15 -0700

    updating port checks

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const smsResponsesHandler = require("./sms-responses-handler");
 const smsSaveResponseHandler = require("./sms-save-handler");
 app.get("/", (req, res) => res.send("Hello World!"));
